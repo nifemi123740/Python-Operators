e2347199d3e20cc9e99f5f0cfee1285bd0ccc049
nifemi123740 <deborahadegbite407@gmail.com>
2025-07-11 20:53:33 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+x=15
+y=20
+
+print(f"Sum of {x} and {y} is {x+y}")
+print(f"Difference of {x} and {y} is {x-y}")
+print(f"Product of {x} and {y} is {x*y}")
+print(f"Quotient of {x} and {y} is {x/y}")
+print(f"Remainder of {x} and {y} is {x%y}")
+print(f"Exponentiation of {x} and {y} is {x**y}")
+print(f"Floor division of {x} and {y} is {x//y}")
+
+print(f"Equality check of {x} and {y} is {x==y}")
+print(f"Greater than check of {x} and {y} is {x>y}")
+print(f"Less than check of {x} and {y} is {x<y}")
+print(f"Is {x} not equal to {y}? {x!=y}")
